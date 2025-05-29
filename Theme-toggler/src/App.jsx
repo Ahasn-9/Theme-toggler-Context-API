95e43b6720dd93936e8f76665992b52a585f00fc
ahasn-9 <mughalahsan718@gmail.com>
2025-05-29 03:41:26 -0700

    theme toggler built using context api

--- Theme-toggler/src/App.jsx
+++ Theme-toggler/src/App.jsx
@@ -1,12 +1,44 @@
-
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './Components/ThemeBtn';
+import Card from './Components/Card';
 
 function App() {
-  
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  useEffect(() => {
+    const htmlElement = document.querySelector('html');
+    htmlElement.classList.remove("dark", "light");
+    htmlElement.classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
-      
-    </>
+  <> 
+    <ThemeProvider value={{
+      themeMode,
+      darkTheme,
+      lightTheme
+    }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn/>
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  </>
   )
 }
 
--- Theme-toggler/tailwind.config.js
+++ Theme-toggler/tailwind.config.js
@@ -1,27 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          light: '#ffffff',
-          dark: '#1a1a1a',
-        },
-        secondary: {
-          light: '#f3f4f6',
-          dark: '#2d2d2d',
-        },
-        text: {
-          light: '#1f2937',
-          dark: '#f3f4f6',
-        }
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
+
+
